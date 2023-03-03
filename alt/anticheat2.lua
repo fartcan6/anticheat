09ae09f0f33e0a284c9a82ddf394bf28c7fc3215
fartcan6 <121769974+fartcan6@users.noreply.github.com>
2023-03-03 18:14:23 +0800

    Update anticheat2.lua

--- alt/anticheat2.lua
+++ alt/anticheat2.lua
@@ -1,1 +1,18 @@
+function onCharacterAdded(character)
+    -- Check if player is in the air
+    if character.Humanoid:GetState() == Enum.HumanoidStateType.Freefall then
+        -- Display message in chat
+        local message = "caught ya lackin. you have been kicked for flying xd."
+
+        local player = game.Players:GetPlayerFromCharacter(character)
+        if player then
+            player:Kick(message)
+            print(message)
+        end
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(onCharacterAdded)
+end)
 
